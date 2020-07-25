0dd5c11e6aa82aedb0831c0f260686b9af017f18
Filipasek <filipasek10@gmail.com>
2020-07-25 21:05:57 +0200

    some edits

---
+++ lib/components/fly.dart
@@ -0,0 +1,23 @@
+import 'dart:ui';
+
+import 'package:Game/game.dart'; //for Canvas
+
+class Fly {
+  final LangawGame game;
+  Rect flyRect;
+  Paint flyPaint;
+
+  Fly(this.game, double x, double y) {
+    flyRect = Rect.fromLTWH(x, y, game.tileSize, game.tileSize);
+    flyPaint = Paint();
+    flyPaint.color = Color(0xff6ab04c);
+  }
+
+  void render(Canvas c) {
+    c.drawRect(flyRect, flyPaint);
+  }
+
+  void update(double t) {
+    //
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flame/util.dart';
 import 'package:flutter/services.dart';
 import 'package:Game/game.dart';
-import 'package:flutter/gestures.dart';
+// import 'package:flutter/gestures.dart';
 
 void main() {
-  BoxGame game = BoxGame();
-  TapGestureRecognizer tapper = TapGestureRecognizer();
-  tapper.onTapDown = game.onTapDown;
+  LangawGame game = LangawGame();
+  // TapGestureRecognizer tapper = TapGestureRecognizer();
+  // tapper.onTapDown = game.onTapDown;
 
   runApp(game.widget);
 
   Util flameUtil = new Util();
   flameUtil.fullScreen();
   flameUtil.setOrientation(DeviceOrientation.portraitUp);
-  flameUtil.addGestureRecognizer(tapper);
+  // flameUtil.addGestureRecognizer(tapper);
 }
